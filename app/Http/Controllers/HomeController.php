fbd1121f378bc3c5030f3842248c7129e5b24826
Junior <duartejusanca@gmail.com>
2021-04-16 09:58:56 +0200

    création base de donné heroku

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
             "subject" => "EpicEvents.fr"
         ];
 
-        DB::table('contacts')->insert([
+        DB::table('contact')->insert([
             'firstname' => $request['firstname'],
             'lastname' => $request['lastname'],
             'email' => $request['email'],
